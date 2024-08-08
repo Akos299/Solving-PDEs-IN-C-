40be6d90c8a578b444be3e6316f80bcc18eae080
LEODASCE SEWANOU <science.ing.guru24.02@gmail.com>
2024-08-08 16:48:10 +0200

    [matrix operations] matrix additions...

--- Iterative-methods/matrix.hpp
+++ Iterative-methods/matrix.hpp
@@ -11,17 +11,38 @@ template<typename T>
 class matT : public vecT<T>{
     private:
     size_t _nrows, _ncols;
+    size_t _ld; // leading dimensions of the matrix we suppose that we are working in column-major
     public:
     matT(size_t nrow, size_t ncol):_nrows(nrow), _ncols(ncol)
     {
-        (*this).set_size(nrow * ncol);
-        (*this).vec_resize(nrow * ncol);
+        _ld = _ncols;
+        this->set_size(nrow * ncol);
+        this->vec_resize(nrow * ncol);
     }
     const T& operator()(int idx, int idy){
-        auto id = idx * _nrows + idy;
+        auto id = idy * _ld + idx;
         return (*this)(id);
     }
+
+    size_t get_leading_dim ()const {return _ld;}
+    size_t get_nb_rows ()const {return _nrows;}
+    size_t get_nb_cols () const {return _ncols;}
 };
+
+// compute A = a*A + b*B
+template<typename T>
+void GEMAd(const matT<T>& A, const matT<T>& B, const T a , const T b);
+
+// add a scalar to all component of a given matrix
+template<typename T>
+void AdScalToMat(const matT<T>& A, const T a);
+
+// generate identity matrix of size n
+template<typename T>
+matT<T> eye(const size_t n);
+
+
+
 
 
 #endif
