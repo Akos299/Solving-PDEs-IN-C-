678b9fa24ffb8ee192dadd4ec6c9e57794193208
Leodasce Sewanou <leodasce.sewanou@ens-lyon.fr>
2024-08-13 17:00:03 +0200

    [matrix operations] overloaded operators declaration

--- Iterative-methods/linear_algebra_op/app/main.cpp
+++ Iterative-methods/linear_algebra_op/app/main.cpp
@@ -7,7 +7,10 @@
 int main(int argc, char** argv)
 {
     vecT<double> vec(5, 10);
-    print_vec<double>(vec);
+    // print_vec<double>(vec.vec_resize(3));
+    auto v = vec.subvector(5,8,2);
+    print_vec<double>(v);
+    std::cout << Norm1<double>(vec);
 
     return 0;
 }
